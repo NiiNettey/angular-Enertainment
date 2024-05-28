250ec6434d0348c13a523c2ceb485cec3b29a372
James Nerquaye-Tetteh <james.nerquaye-tetteh@amalitech.com>
2024-05-28 09:47:36 +0000

    feat add home

--- entertainment-app/src/app/app.routes.ts
+++ entertainment-app/src/app/app.routes.ts
@@ -4,4 +4,5 @@ export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
   { path: 'signup', loadComponent: () => import('./signup/signup.component').then(m => m.SignupComponent) },
+  { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) }
 ];
--- entertainment-app/src/main.ts
+++ entertainment-app/src/main.ts
@@ -5,7 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { routes } from './app/app.routes';
 
-platformBrowserDynamic().bootstrapModule(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, RouterModule.forRoot(routes))]
-})
+platformBrowserDynamic().bootstrapModule(AppComponent, )
   .catch(err => console.error(err));
+
+
+
+  // {
+  //   providers: [importProvidersFrom(BrowserModule, RouterModule.forRoot(routes))]
+  // }
